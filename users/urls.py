8e83851f2c91f8f72eb2bd3655375799657af5b7
smereddy <suryamereddy@gmail.com>
2021-09-12 21:56:52 -0700

    frontend changes

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # from . import views
-from .views import SignUpView
+from .views import SignUpView, change_password
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('^password/', change_password, name='change_password'),
 ]
